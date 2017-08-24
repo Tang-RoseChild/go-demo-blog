3147679b7d1771f8ddf019fecd55e48ed00c49bf
tangshiyou <tang.rosechild@gmail.com>
2017-08-24 16:00:35 +0800

    add gzip support for static files

--- cmd/entry/main.go
+++ cmd/entry/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"compress/gzip"
 	"flag"
 	"fmt"
 	"net/http"
+	"path"
 
+	"github.com/NYTimes/gziphandler"
 	"github.com/gin-gonic/gin"
 
 	"github.com/Tang-RoseChild/go-demo-blog/account"
@@ -16,8 +19,9 @@ import (
 )
 
 var (
-	addr   = flag.String("addr", ":9991", "addr")
-	secret = flag.String("secret", "就不告诉你", "secret")
+	addr          = flag.String("addr", ":9991", "addr")
+	secret        = flag.String("secret", "就不告诉你", "secret")
+	compressLevel = flag.Int("level", gzip.BestSpeed, " compression level")
 )
 
 func main() {
@@ -27,7 +31,8 @@ func main() {
 	// dbutils.DB.LogMode(true) // for testing
 
 	engine := NewGinEngine()
-	engine.StaticFS("/", http.Dir("./static"))
+	// engine.StaticFS("/", http.Dir("./static"))
+	createGzipStaticFS(engine, *compressLevel, "/", http.Dir("./static"))
 	LoadHandler(engine)
 
 	fmt.Println("listen ..... " + *addr)
@@ -46,4 +51,20 @@ func LoadHandler(engine *gin.Engine) {
 	account.GinLoad(r)
 	blog.GinLoad(r)
 	comments.GinLoad(r)
+}
+
+// same with engine.StaticFS, just adding gzip
+func createGzipStaticFS(engine *gin.Engine, compressionLevel int, relativePath string, fs http.FileSystem) gin.HandlerFunc {
+	compressionLevelWrap, err := gziphandler.GzipHandlerWithOpts(gziphandler.CompressionLevel(compressionLevel))
+	if err != nil {
+		panic(err)
+	}
+	fileServer := compressionLevelWrap(http.StripPrefix(relativePath, http.FileServer(fs)))
+	urlPattern := path.Join("/", "/*filepath")
+	handler := func(c *gin.Context) {
+		fileServer.ServeHTTP(c.Writer, c.Request)
+	}
+	engine.GET(urlPattern, handler)
+	engine.HEAD(urlPattern, handler)
+	return handler
 }
